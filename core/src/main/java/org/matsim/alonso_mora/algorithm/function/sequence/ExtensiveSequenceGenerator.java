cd20c30adf8ea723bd5d4f935ca164078f5199e1
tkchouaki <tkchouaki@gmail.com>
2026-01-23 16:07:50 +0100

    feat: caching the AlonsoMoreStop List returned by get()

--- core/src/main/java/org/matsim/alonso_mora/algorithm/function/sequence/ExtensiveSequenceGenerator.java
+++ core/src/main/java/org/matsim/alonso_mora/algorithm/function/sequence/ExtensiveSequenceGenerator.java
@@ -1,10 +1,6 @@
 package org.matsim.alonso_mora.algorithm.function.sequence;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -15,62 +11,67 @@ import org.matsim.alonso_mora.algorithm.AlonsoMoraVehicle;
 
 /**
  * The extensive sequence generator as described by Alonso-Mora et al.
- * 
+ * <p>
  * The generator loops through all possible combinations of pickup and dropoff
  * stops given the onboard and new requests for the vehicle.
- * 
+ *
  * @author sebhoerl
  */
 public class ExtensiveSequenceGenerator implements SequenceGenerator {
-	private int[] currentSequence;
-	private int currentIndex;
+    private int[] currentSequence;
+    private int currentIndex;
 
-	private int sequenceLength;
+	private final int sequenceLength;
 
 	private int[] requestIndices;
 	private boolean[] pickupIndices;
 
-	private final List<AlonsoMoraStop> stops;
-	private Set<Integer> onboardIndices = new HashSet<>();
+    private final List<AlonsoMoraStop> stops;
+    private Set<Integer> onboardIndices = new HashSet<>();
 
-	private boolean finished = false;
+    private boolean finished = false;
 
-	public ExtensiveSequenceGenerator(Collection<AlonsoMoraRequest> onboardRequests,
-			Collection<AlonsoMoraRequest> requests) {
-		this.sequenceLength = onboardRequests.size() + requests.size() * 2;
 
-		this.currentSequence = new int[this.sequenceLength];
-		this.currentIndex = 0;
+    private boolean isStopsCacheValid = false;
+    private List<AlonsoMoraStop> stopsCache;
 
-		this.requestIndices = new int[sequenceLength];
-		this.pickupIndices = new boolean[sequenceLength];
 
-		this.stops = new ArrayList<>(this.sequenceLength);
+    public ExtensiveSequenceGenerator(Collection<AlonsoMoraRequest> onboardRequests,
+                                      Collection<AlonsoMoraRequest> requests) {
+        this.sequenceLength = onboardRequests.size() + requests.size() * 2;
 
-		int requestIndex = 0;
-		int stopIndex = 0;
+        this.currentSequence = new int[this.sequenceLength];
+        this.currentIndex = 0;
 
-		for (AlonsoMoraRequest request : onboardRequests) {
-			onboardIndices.add(requestIndex);
+        this.requestIndices = new int[sequenceLength];
+        this.pickupIndices = new boolean[sequenceLength];
 
-			requestIndices[stopIndex] = requestIndex;
-			pickupIndices[stopIndex] = false;
-			stops.add(new AlonsoMoraStop(StopType.Dropoff, request.getDropoffLink(), request));
-			stopIndex++;
+        this.stops = new ArrayList<>(this.sequenceLength);
 
-			requestIndex++;
-		}
+        int requestIndex = 0;
+        int stopIndex = 0;
 
-		for (AlonsoMoraRequest request : requests) {
-			requestIndices[stopIndex] = requestIndex;
-			pickupIndices[stopIndex] = true;
-			stops.add(new AlonsoMoraStop(StopType.Pickup, request.getPickupLink(), request));
-			stopIndex++;
+        for (AlonsoMoraRequest request : onboardRequests) {
+            onboardIndices.add(requestIndex);
 
-			requestIndices[stopIndex] = requestIndex;
-			pickupIndices[stopIndex] = false;
-			stops.add(new AlonsoMoraStop(StopType.Dropoff, request.getDropoffLink(), request));
-			stopIndex++;
+            requestIndices[stopIndex] = requestIndex;
+            pickupIndices[stopIndex] = false;
+            stops.add(new AlonsoMoraStop(StopType.Dropoff, request.getDropoffLink(), request));
+            stopIndex++;
+
+            requestIndex++;
+        }
+
+        for (AlonsoMoraRequest request : requests) {
+            requestIndices[stopIndex] = requestIndex;
+            pickupIndices[stopIndex] = true;
+            stops.add(new AlonsoMoraStop(StopType.Pickup, request.getPickupLink(), request));
+            stopIndex++;
+
+            requestIndices[stopIndex] = requestIndex;
+            pickupIndices[stopIndex] = false;
+            stops.add(new AlonsoMoraStop(StopType.Dropoff, request.getDropoffLink(), request));
+            stopIndex++;
 
 			requestIndex++;
 		}
@@ -78,16 +79,20 @@ public class ExtensiveSequenceGenerator implements SequenceGenerator {
 		internalAdvance(false);
 	}
 
-	@Override
-	public void advance() {
-		internalAdvance(true);
-	}
+    private void invalidateStopsCache() {
+        this.isStopsCacheValid = false;
+    }
 
-	@Override
-	public void abort() {
-		internalAbort();
-		internalAdvance(false);
-	}
+    @Override
+    public void advance() {
+        internalAdvance(true);
+    }
+
+    @Override
+    public void abort() {
+        internalAbort();
+        internalAdvance(false);
+    }
 
 	void internalAdvance(boolean dropCurrent) {
 		while (!finished && (!isFeasible() || dropCurrent)) {
@@ -100,24 +105,27 @@ public class ExtensiveSequenceGenerator implements SequenceGenerator {
 				internalAbort();
 			}
 
-			dropCurrent = false;
-		}
-	}
+            dropCurrent = false;
+        }
+        invalidateStopsCache();
+    }
 
 	void internalAbort() {
 		while (currentIndex > 0) {
 			currentIndex--;
 
-			if (currentSequence[currentIndex] < sequenceLength - 1) {
-				currentSequence[currentIndex]++;
-				return;
-			}
-		}
+            if (currentSequence[currentIndex] < sequenceLength - 1) {
+                currentSequence[currentIndex]++;
+                invalidateStopsCache();
+                return;
+            }
+        }
 
-		if (currentSequence[0] < sequenceLength - 1) {
-			currentSequence[0]++;
-			return;
-		}
+        if (currentSequence[0] < sequenceLength - 1) {
+            currentSequence[0]++;
+            invalidateStopsCache();
+            return;
+        }
 
 		finished = true;
 	}
@@ -147,30 +155,34 @@ public class ExtensiveSequenceGenerator implements SequenceGenerator {
 			}
 		}
 
-		return true;
+        return true;
 	}
 
-	@Override
-	public boolean hasNext() {
-		return !finished;
-	}
+    @Override
+    public boolean hasNext() {
+        return !finished;
+    }
 
-	@Override
-	public boolean isComplete() {
-		return currentIndex == sequenceLength - 1;
-	}
+    @Override
+    public boolean isComplete() {
+        return currentIndex == sequenceLength - 1;
+    }
 
-	@Override
-	public List<AlonsoMoraStop> get() {
-		return IntStream.of(currentSequence).limit(currentIndex + 1).mapToObj(i -> stops.get(i))
-				.collect(Collectors.toList());
-	}
+    @Override
+    public List<AlonsoMoraStop> get() {
+        if (!this.isStopsCacheValid) {
+            this.stopsCache = IntStream.of(currentSequence).limit(currentIndex + 1).mapToObj(i -> stops.get(i))
+                    .collect(Collectors.toList());
+            this.isStopsCacheValid = true;
+        }
+        return this.stopsCache;
+    }
 
-	static public class Factory implements SequenceGeneratorFactory {
-		@Override
-		public SequenceGenerator createGenerator(AlonsoMoraVehicle vehicle,
-				Collection<AlonsoMoraRequest> onboardRequests, Collection<AlonsoMoraRequest> requests, double now) {
-			return new ExtensiveSequenceGenerator(onboardRequests, requests);
-		}
-	}
+    static public class Factory implements SequenceGeneratorFactory {
+        @Override
+        public SequenceGenerator createGenerator(AlonsoMoraVehicle vehicle,
+                                                 Collection<AlonsoMoraRequest> onboardRequests, Collection<AlonsoMoraRequest> requests, double now) {
+            return new ExtensiveSequenceGenerator(onboardRequests, requests);
+        }
+    }
 }
